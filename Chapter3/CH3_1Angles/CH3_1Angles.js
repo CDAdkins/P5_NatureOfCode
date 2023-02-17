81d76e18a8222ced6031471cc50c2dd92f9e7065
chris <adkinscda@gmail.com>
2023-02-16 20:10:43 -0800

    Minor Updates

--- Chapter3/CH3_1Angles/CH3_1Angles.js
+++ Chapter3/CH3_1Angles/CH3_1Angles.js
@@ -1,21 +1,27 @@
 let i;
+let speed;
 function setup() {
+
+  frameRate(60)
   createCanvas(windowWidth, windowHeight);
   background(24);
-  i = 10;
+  i = 0;
+  //speed = 0.017;
+  speed = 0.1;
   textSize(24);
 }
 
 function draw() {
-  i *= 1.0015;
-  background(24, 24, 24, i);
+ //i *= 1.0015;
+  i += speed
+  background(24, 24, 24, 255);
   noStroke();
   fill(240, 100, 160);
   rectMode(CENTER);
-  text("Rotating object at " + round(i) + " radians/frame", 10, 30);
+  text("Rotating object at " + speed*60 + " radians/second", 10, 30);
 
 
   translate(width/2, height/2)
   rotate(i);
-  rect(0, 0, width/3, 50);
+  rect(0, 0, width/2, 10);
 }
